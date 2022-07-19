d6f080265d0598063116a9602d721066d770cfe1
Daniel Alves <danielresende2000@gmail.com>
2022-07-19 19:28:22 -0300

    att

--- TP/include/TreeNode.hpp
+++ TP/include/TreeNode.hpp
@@ -13,10 +13,10 @@ class TreeNode{
 		int height;
 	public:
 		TreeNode(MsgContainer dataInput) : data(dataInput), leftPtr(nullptr), rightPtr(nullptr), height(1) {
-			escreveMemLog((long int)(this), sizeof(TreeNode*), 0);
+			escreveMemLog((long int)(&data), sizeof(MsgContainer), 0);
 		}
-		MsgContainer getData() { leMemLog((long int)(this), sizeof(TreeNode*), 0); return data; }
-		void setData(MsgContainer newData) { data = newData; escreveMemLog((long int)(this), sizeof(TreeNode*), 0); }
+		MsgContainer getData() { leMemLog((long int)(&data), sizeof(MsgContainer), 0); return data; }
+		void setData(MsgContainer newData) { data = newData; escreveMemLog((long int)(&data), sizeof(MsgContainer), 0); }
 		void setHeight(int H) { height = H; }
 	friend class AVLTree;
 };
